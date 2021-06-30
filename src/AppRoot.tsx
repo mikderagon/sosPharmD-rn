b3763a6775691d769cb8cbfdbffc42727218f113
MikaelDeragon <mikael@deragon.dev>
2021-06-30 16:06:24 -0400

    dark status bar

--- src/AppRoot.tsx
+++ src/AppRoot.tsx
@@ -6,8 +6,8 @@
  * @flow strict-local
  */
 
-import React, { createContext, useContext } from 'react';
-import { StyleSheet, useColorScheme } from 'react-native';
+import React, { createContext, useContext, useState } from 'react';
+import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
 import 'react-native-gesture-handler';
 import Navigator from './Navigator';
 import { StateProvider, store } from './store';
@@ -16,16 +16,10 @@ const AppRoot = () => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <StateProvider>
+      <StatusBar animated barStyle="dark-content" showHideTransition="fade" />
       <Navigator />
     </StateProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    width: '100%',
-  },
-});
 
 export default AppRoot;
